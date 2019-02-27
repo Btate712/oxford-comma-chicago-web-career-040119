adce56428919787195a91504411232399f040abb
Robert Tate <btate712@gmail.com>
2019-02-27 13:13:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array.join
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join (" and ")
+  else
+    temp = array.pop
+    array.join(", ")
+    array.push("and")
+    array.push(temp)
 end
